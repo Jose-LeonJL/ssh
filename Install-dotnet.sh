8a20698eb89d86fade029c75b71900886203cdc5
Jose_LeonJL <60480200+Jose-LeonJL@users.noreply.github.com>
2022-09-22 16:17:29 -0600

    Add install-dotnet.sh

---
+++ Install-dotnet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Instalando Dotnet 6.0...
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
+echo Dotnet Instalado con exito !!!
